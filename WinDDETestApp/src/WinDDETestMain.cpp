fe9f86c3f1da7d53a08802268993cbc04edba78e
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2018-02-26 22:14:08 +0000

    Formatting

--- WinDDETestApp/src/WinDDETestMain.cpp
+++ WinDDETestApp/src/WinDDETestMain.cpp
@@ -1,9 +1,9 @@
-/*************************************************************************\ 
-* Copyright (c) 2018 Science and Technology Facilities Council (STFC), GB. 
-* All rights reverved. 
-* This file is distributed subject to a Software License Agreement found 
-* in the file LICENSE.txt that is included with this distribution. 
-\*************************************************************************/ 
+/*************************************************************************\
+* Copyright (c) 2018 Science and Technology Facilities Council (STFC), GB.
+* All rights reverved.
+* This file is distributed subject to a Software License Agreement found
+* in the file LICENSE.txt that is included with this distribution.
+\*************************************************************************/
 
 #include <windows.h>
 #include <stddef.h>
@@ -26,45 +26,45 @@
 //
 // Any IOC wanting to be a DDE server will need to have
 // its main program changed in a similar was to run a message pump
- 
+
 static void iocMain(void* arg)
 {
-    iocsh((const char*)arg);
-    epicsExit(0);
+	iocsh((const char*)arg);
+	epicsExit(0);
 }
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-    if(argc>=2) {    
-        iocsh(argv[1]);
-        epicsThreadSleep(.2);
-    }
+	if (argc >= 2) {
+		iocsh(argv[1]);
+		epicsThreadSleep(.2);
+	}
 	// start interactive ioc shell in separate thread to keep main thread for use as message pump
 	_beginthread(iocMain, 0, NULL);
 
 	// standard windows message pump to process DDE messages
 	MSG msg;
-    BOOL bRet; 
-    while( (bRet = GetMessage( &msg, NULL, 0, 0 )) != 0 )
-    { 
-        if (bRet == -1)
-        {
-            // handle the error and possibly exit
+	BOOL bRet;
+	while ((bRet = GetMessage(&msg, NULL, 0, 0)) != 0)
+	{
+		if (bRet == -1)
+		{
+			// handle the error and possibly exit
 			printf("win message error\n");
-        }
-        else
-        {
+		}
+		else
+		{
 			if (msg.message == WM_EPICSDDE) // special message used to enabel us to notify DDE of parameter changes
 			{
 				(*(dde_cb_func_t)msg.wParam)(msg.lParam); // will call WinDDEDriver::postAdvise() with given parameter
 			}
 			else
 			{
-                TranslateMessage(&msg); 
-                DispatchMessage(&msg);
-			}				
-        }
-    } 
-    epicsExit(0);
-    return(0);
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+	}
+	epicsExit(0);
+	return(0);
 }
