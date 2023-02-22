26fbb38d4f06cc52aa27c2231d2829ad56e50726
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-02-22 23:58:45 +0000

    Add check on multiple processes

--- WinDDEApp/src/run_windde.bat
+++ WinDDEApp/src/run_windde.bat
@@ -1,3 +1,12 @@
+@echo off
+tasklist /fi "ImageName eq WinDDEClient.exe" /fo csv 2>NUL | find /I "WinDDEClient.exe">NUL
+if "%ERRORLEVEL%"=="0" (
+    echo Program WinDDEClient is already running in another window
+	echo If that one is not working properly, please close that window
+	echo and try running this file again
+	pause
+	exit /b 1
+)
 :LOOP
 %~dp0WinDDEClient.exe
 goto LOOP
